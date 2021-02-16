351cf333187a72976ffbbb371d0a0f72f1a3dd7f
benjs <benjs@benjs.de>
2021-02-16 11:03:55 +0100

    Added dataset

---
+++ dataset.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+from typing import Callable
+from torchvision import transforms
+from torch.utils.data.dataset import Dataset
+from torchvision.datasets import ImageNet
+
+def get_dataset(path:Path, transforms:Callable) -> Dataset:
+    return ImageNet(str(path), split='val', transform=transforms)
