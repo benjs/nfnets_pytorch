5ced1d41d62fd8c19a2892114b1b598e49706d0f
benjs <benjs@benjs.de>
2021-02-16 16:15:05 +0100

    Added parameter to config file

--- train.py
+++ train.py
@@ -1,25 +1,30 @@
-import pathlib
-import torch
 import yaml
 import argparse
 from pathlib import Path
 
-from model import NFNet
-
-from torch import optim
+import torch
 import torch.nn as nn
-
-import torch.nn.functional as F
 from torch.utils.data import Subset
 from torch.utils.data.dataloader import DataLoader
 from torchvision.transforms.transforms import Compose, Normalize, Resize, ToTensor
-from torchvision.models.resnet import resnet50
+
+import optim
+from model import NFNet
 from dataset import get_dataset
 
-def train(variant:str, num_classes:int, batch_size:int, 
-    epochs:int, learning_rate:float, dataset_path:Path, device:str) -> None:
+def train(variant:str, num_classes:int, batch_size:int, epochs:int, dataset_path:Path, 
+    learning_rate:float=0.1, device:str='cpu', stochdepth_rate:float=None, alpha:float=0.2,
+    group_size:int=1, se_ratio:float=0.5, scale_lr:bool=True, momentum:float=0.9,
+    weight_decay:float=2e-5, nesterov:bool=True, clipping:float=0.1, num_workers:int=0) -> None:
     
-    model = NFNet(num_classes=num_classes, variant=variant)
+    model = NFNet(
+        num_classes=num_classes, 
+        variant=variant, 
+        stochdepth_rate=stochdepth_rate, 
+        alpha=alpha,
+        se_ratio=se_ratio,
+        groups=group_size,
+        )
 
     transforms = Compose([
         Resize((model.train_imsize, model.train_imsize)),
@@ -31,20 +36,24 @@ def train(variant:str, num_classes:int, batch_size:int,
     subset = Subset(dataset, range(batch_size))
 
     dataloader = DataLoader(
-        dataset=subset, 
+        dataset=dataset, 
         batch_size=batch_size, #=batch_size
-        shuffle=False,
-        num_workers=0, 
+        shuffle=True,
+        num_workers=num_workers, 
         pin_memory=True)
 
     model.to(device)
 
-    optimizer = optim.SGD(
+    if scale_lr:
+        learning_rate = learning_rate*batch_size/256
+
+    optimizer = optim.SGD_AGC(
         model.parameters(), 
-        lr=learning_rate, 
-        momentum=0.9, 
-        #weight_decay=2e-5, 
-        #nesterov=True
+        lr=learning_rate*batch_size/256 if scale_lr else learning_rate, 
+        momentum=momentum,
+        clipping=clipping,
+        weight_decay=weight_decay, 
+        nesterov=nesterov
         )
 
     ce_loss = nn.CrossEntropyLoss()
@@ -52,7 +61,7 @@ def train(variant:str, num_classes:int, batch_size:int,
     for epoch in range(epochs):
         model.train()
 
-        for _, data in enumerate(dataloader):
+        for step, data in enumerate(dataloader):
             inputs = data[0].to(device)
             targets = data[1].to(device)
 
@@ -63,12 +72,14 @@ def train(variant:str, num_classes:int, batch_size:int,
             loss.backward()
             optimizer.step()
 
-            print(f"\rEpoch {epoch+1}/{epochs}: l={loss.item()}", sep=' ', end='', flush=True)
+            print(f"\rEpoch {epoch+1:04d}/{epochs}\tImg: {step*batch_size:5d}/{len(dataloader.dataset)}\tLoss: {loss.item():8.6f}                 ", 
+                sep=' ', end='', flush=True)
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Train NFNets.')
     parser.add_argument('--config', type=Path, help='Path to config.yaml', default='default_config.yaml')
+    parser.add_argument('--overfit')
     args = parser.parse_args()
     
     if not args.config.exists():
@@ -86,4 +97,13 @@ if __name__=='__main__':
         epochs=config['epochs'],
         learning_rate=config['learning_rate'],
         device=config['device'],
+        alpha=config['alpha'],
+        stochdepth_rate=config['stochdepth_rate'],
+        se_ratio=config['se_ratio'],
+        group_size=config['groups'],
+        momentum=config['momentum'],
+        weight_decay=config['weight_decay'],
+        nesterov=config['nesterov'],
+        clipping=config['clipping'],
+        num_workers=config['num_workers'],
         )
