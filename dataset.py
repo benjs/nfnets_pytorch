bf4674c64334fdc8beac02a9fe903df28578293d
benjs <benjs@benjs.de>
2021-02-19 16:03:20 +0100

    Add default

--- dataset.py
+++ dataset.py
@@ -4,5 +4,5 @@ from torchvision import transforms
 from torch.utils.data.dataset import Dataset
 from torchvision.datasets import ImageNet
 
-def get_dataset(path:Path, transforms:Callable) -> Dataset:
+def get_dataset(path:Path, transforms:Callable=None) -> Dataset:
     return ImageNet(str(path), split='val', transform=transforms)
